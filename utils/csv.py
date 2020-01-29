5f4561e74bed8f6154483dea966c10ec5eec843f
Jose Ruiz <chepe.ruiz@gmail.com>
2020-01-28 21:24:42 -0800

    csv parallel methods

--- utils/csv.py
+++ utils/csv.py
@@ -1,6 +1,6 @@
 # pylint: disable=no-name-in-module
 # pylint: disable=import-error
-
+import os
 import re
 import csv
 from multiprocessing import cpu_count, Pool
@@ -8,62 +8,19 @@ from multiprocessing import cpu_count, Pool
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
-from utils.string import clean_string, title_to_snake, first_valid
+from utils.string import clean_string, title_to_snake, first_valid, remove_accents
 
 URL_CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ.-_~+#,%&=*;:@/"
-EXTRA_STOPWORDS = {
-    "another",
-    "april",
-    "august",
-    "bug",
-    "call",
-    "core",
-    "community.nxp.com",
-    "december",
-    "down",
-    "eight",
-    "existing",
-    "february",
-    "five",
-    "fix",
-    "follow",
-    "four",
-    "friday",
-    "help",
-    "january",
-    "july",
-    "june",
-    "left",
-    "list",
-    "march",
-    "monday",
-    "new",
-    "nine",
-    "november",
-    "october",
-    "one",
-    "order",
-    "plan",
-    "research",
-    "review",
-    "right",
-    "saturday",
-    "september",
-    "seven",
-    "six",
-    "start",
-    "stop",
-    "sunday",
-    "ten",
-    "three",
-    "thursday",
-    "tuesday",
-    "two",
-    "up",
-    "using",
-    "view",
-    "wednesday",
-}
+EXTRA_STOPWORDS_PATH = os.environ["PROJECT_SRC"] + "/nlp/stopwords.txt"
+
+
+def extra_stopwords(stop_words_path=EXTRA_STOPWORDS_PATH):
+    try:
+        with open(stop_words_path) as f:
+            sw = f.read().split("\n")
+        return sw
+    except Exception as e:
+        return set()
 
 
 def line_generator(lines):
@@ -77,35 +34,41 @@ def clean_headers(headers):
     return headers
 
 
-is_empty = lambda s: not any(s.replace("'", "").replace('"', "").replace("\n", "").split(","))
-
-
-def chunk_generator(lines, n=cpu_count() * 4):
-    g = line_generator(lines)
-    try:
-        while True:
-            lines = []
-            for _ in range(n):
-                lines.append(next(g))
-            yield lines
-    except StopIteration:
-        if len(lines) > 0:
-            yield lines
-        return
+def chunk_generator(reader, chunk_size=16):
+    while True:
+        try:
+            chunk = []
+            for _ in range(chunk_size):
+                chunk.append(next(reader))
+            yield chunk
+        except StopIteration:
+            return chunk
 
 
 def filter_empty(lines):
-    return [line for line in lines if not is_empty(line)]
+    return [line for line in lines if any(line)]
 
 
-def get_stopwords(default_stopwords=set(stopwords.words("english")), extra_stopwords=EXTRA_STOPWORDS):
-    return default_stopwords.union(extra_stopwords)
+def get_stopwords(default_stopwords=set(stopwords.words("english")), extra_stopwords=extra_stopwords()):
+    sw = default_stopwords.union(extra_stopwords)
+    return sw
 
 
-def remove_stopwords(line, stop_words=get_stopwords()):
-    words = word_tokenize(line)
+def remove_stopwords(text, stop_words=get_stopwords(), remove_numbers=False):
+    if type(text) is bytes:
+        text = text.decode("utf-8")
+    text = clean_string(text)
+    words = word_tokenize(text)
     words = [word for word in words if word.lower() not in stop_words]
-    return fix_spaces(" ".join(words))
+    if remove_numbers:
+        words = [
+            word for word in words if not word.replace(".", "").replace(":", "").replace("x", "").isnumeric()
+        ]
+    return " ".join(words)
+
+
+def line_remove_stopwords(line):
+    return [remove_stopwords(cell.lower()) for cell in line]
 
 
 def fix_spaces(text, charset='":;,.<>/?[]{}\\|"'):
@@ -114,13 +77,30 @@ def fix_spaces(text, charset='":;,.<>/?[]{}\\|"'):
     return text.strip()
 
 
+def line_fix_spaces(lines):
+    return [fix_spaces(l) for l in lines]
+
+
+def parallel_clean_csv(dirty_path, clean_path):
+    reader = csv.reader(open(dirty_path))
+    writer = csv.writer(open(clean_path, "w"))
+    headers = clean_headers(next(reader))
+    writer.writerow(headers)
+    with Pool(cpu_count()) as p:
+        for chunk in chunk_generator(reader):
+            new_lines = p.map(line_remove_stopwords, chunk)
+            new_lines = p.map(line_fix_spaces, new_lines)
+            new_lines = filter_empty(new_lines)
+            writer.writerows(new_lines)
+
+
 def clean_csv(dirty_path, clean_path):
     reader = csv.reader(open(dirty_path))
     writer = csv.writer(open(clean_path, "w"))
     headers = clean_headers(next(reader))
     writer.writerow(headers)
     for line in reader:
-        new_line = [remove_stopwords(cell.lower()) for cell in line]
+        new_line = [fix_spaces(remove_stopwords(cell.lower())) for cell in line]
         if any(new_line):
             writer.writerow(new_line)
 
