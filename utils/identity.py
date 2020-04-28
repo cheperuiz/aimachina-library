7ae7d5eb75fabf782aa6c6d7c45d463f4319452d
Fabian Duque <fabianduque@Fabians-MacBook-Pro.local>
2020-04-28 17:17:14 -0500

    fixing cookies bug

--- utils/identity.py
+++ utils/identity.py
@@ -17,7 +17,7 @@ def get_kratos_user_identity(request, type='email'):
 
         kratos_user_identity = requests.get(url, headers=headers)
         return kratos_user_identity.json()["identity"]["traits"]["email"] if type == 'email' else kratos_user_identity.json()["identity"]["id"]
-    elif request.cookies == {} and environ['ENVIRONMENT'] == 'development':
+    elif environ['ENVIRONMENT'] == 'development':
         return environ['DEVELOPMENT_USER_ID']
     else:
         return 400
